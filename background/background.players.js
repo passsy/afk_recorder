7837b6ef54943d4233caeb1299ecffe4a468ff72
Pascal Welsch <pascal@welsch.dev>
2020-03-21 14:04:15 +0100

    Reformat

--- background/background.players.js
+++ background/background.players.js
@@ -1,10 +1,10 @@
 var videoPlayers = [];
 
 function initVideoPlayer(stream) {
-    var videoPlayer = document.createElement('video');
-    videoPlayer.muted = !enableTabCaptureAPI;
-    videoPlayer.volume = !!enableTabCaptureAPI;
-    videoPlayer.srcObject = stream;
+  var videoPlayer = document.createElement("video");
+  videoPlayer.muted = !enableTabCaptureAPI;
+  videoPlayer.volume = !!enableTabCaptureAPI;
+  videoPlayer.srcObject = stream;
 
-    videoPlayers.push(videoPlayer);
+  videoPlayers.push(videoPlayer);
 }
--- preview/preview.php.upload.js
+++ preview/preview.php.upload.js
@@ -1,140 +1,158 @@
-var server_url = 'https://your-domain.com/f/';
+var server_url = "https://your-domain.com/f/";
 
-document.querySelector('#btn-php-upload').onclick = function() {
-    if (!file) {
-        fname.innerHTML = 'You did NOT record anything yet.';
-        return;
+document.querySelector("#btn-php-upload").onclick = function() {
+  if (!file) {
+    fname.innerHTML = "You did NOT record anything yet.";
+    return;
+  }
+
+  this.disabled = true;
+
+  fresolutions.innerHTML = fsize.innerHTML = fduration.innerHTML = browserCache.innerHTML =
+    "";
+  fname.innerHTML = "Upload started...";
+
+  uploadToPHPServer(file, function(progress, videoURL) {
+    browserCache.innerHTML = "";
+
+    if (progress === "ended" || videoURL) {
+      showPHPURL(videoURL);
+      document.title = "Upload successful";
+      return;
     }
 
-    this.disabled = true;
+    if (progress != "Upload started...") {
+      fname.innerHTML = "Upload Progress: " + progress + "%";
+      browserCache.innerHTML =
+        "<progress min=0 max=100 value=" +
+        progress +
+        ' style="margin-top: 10px;"></progress>';
+    } else {
+      fname.innerHTML = progress;
+    }
 
-    fresolutions.innerHTML = fsize.innerHTML = fduration.innerHTML = browserCache.innerHTML = '';
-    fname.innerHTML = 'Upload started...';
+    document.title = progress + "% uploaded";
 
-    uploadToPHPServer(file, function(progress, videoURL) {
-        browserCache.innerHTML = '';
-
-        if (progress === 'ended' || videoURL) {
-            showPHPURL(videoURL);
-            document.title = 'Upload successful';
-            return;
-        }
-
-        if (progress != 'Upload started...') {
-            fname.innerHTML = 'Upload Progress: ' + progress + '%';
-            browserCache.innerHTML = '<progress min=0 max=100 value=' + progress + ' style="margin-top: 10px;"></progress>';
-        } else {
-            fname.innerHTML = progress;
-        }
-
-        document.title = progress + '% uploaded';
-
-        if (progress >= 99 || videoURL || progress === 'progress-ended') {
-            browserCache.innerHTML = '';
-            fname.innerHTML = 'Uploaded to Server. Retrieving the private video URL...';
-        }
-    });
+    if (progress >= 99 || videoURL || progress === "progress-ended") {
+      browserCache.innerHTML = "";
+      fname.innerHTML =
+        "Uploaded to Server. Retrieving the private video URL...";
+    }
+  });
 };
 
 function showPHPURL(videoURL) {
-    DiskStorage.UpdateFileInfo(file.name, {
-        php: videoURL
-    }, function() {
-        file.url = videoURL;
-        if(!file.item) file.item = {};
-        file.item.php = videoURL;
-        onGettingFile(file, file.item);
-    });
+  DiskStorage.UpdateFileInfo(
+    file.name,
+    {
+      php: videoURL
+    },
+    function() {
+      file.url = videoURL;
+      if (!file.item) file.item = {};
+      file.item.php = videoURL;
+      onGettingFile(file, file.item);
+    }
+  );
 }
 
 function uploadToPHPServer(blob, callback) {
-    // create FormData
-    var formData = new FormData();
+  // create FormData
+  var formData = new FormData();
 
-    var fName = blob.name;
+  var fName = blob.name;
 
-    formData.append('video-filename', fName);
-    formData.append('video-blob', blob);
+  formData.append("video-filename", fName);
+  formData.append("video-blob", blob);
 
-    callback('Uploading recorded-file to server.');
+  callback("Uploading recorded-file to server.");
 
-    makeXMLHttpRequest(server_url, formData, function(progress) {
-        if (progress === 'upload-faild') {
-            return;
-        }
+  makeXMLHttpRequest(server_url, formData, function(progress) {
+    if (progress === "upload-faild") {
+      return;
+    }
 
-        if (progress !== 'http-response-200') {
-            callback(progress);
-            return;
-        }
+    if (progress !== "http-response-200") {
+      callback(progress);
+      return;
+    }
 
-        callback('ended', server_url + fName);
-    });
+    callback("ended", server_url + fName);
+  });
 }
 
 function deleteFromPHPServer(fName, callback) {
-    // create FormData
-    var formData = new FormData();
+  // create FormData
+  var formData = new FormData();
 
-    formData.append('delete-file', fName);
+  formData.append("delete-file", fName);
 
-    makeXMLHttpRequest(server_url + 'delete.php', formData, function(progress) {
-        if (progress === 'deleted' || progress === 'Problem deleting file.') {
-            callback(progress, fName)
-        }
-    });
+  makeXMLHttpRequest(server_url + "delete.php", formData, function(progress) {
+    if (progress === "deleted" || progress === "Problem deleting file.") {
+      callback(progress, fName);
+    }
+  });
 }
 
 function makeXMLHttpRequest(url, data, callback) {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState == 4 && request.status == 200) {
-            if (request.responseText && request.responseText.toString().indexOf('<h2>Upload failed.</h2>') === 0) {
-                fname.innerHTML = request.responseText;
-                header.style.height = 'auto';
-                header.style.color = 'red';
-                callback('upload-faild');
-                return;
-            }
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      if (
+        request.responseText &&
+        request.responseText.toString().indexOf("<h2>Upload failed.</h2>") === 0
+      ) {
+        fname.innerHTML = request.responseText;
+        header.style.height = "auto";
+        header.style.color = "red";
+        callback("upload-faild");
+        return;
+      }
 
-            if (request.responseText && request.responseText.toString().indexOf('deleted successfully') !== -1) {
-                callback('deleted')
-                return;
-            }
+      if (
+        request.responseText &&
+        request.responseText.toString().indexOf("deleted successfully") !== -1
+      ) {
+        callback("deleted");
+        return;
+      }
 
-            if (request.responseText && request.responseText.toString() === 'Problem deleting file.') {
-                callback(request.responseText)
-                return;
-            }
+      if (
+        request.responseText &&
+        request.responseText.toString() === "Problem deleting file."
+      ) {
+        callback(request.responseText);
+        return;
+      }
 
-            callback('http-response-200');
-        }
-    };
+      callback("http-response-200");
+    }
+  };
 
-    request.upload.onloadstart = function() {
-        callback('Upload started...');
-    };
+  request.upload.onloadstart = function() {
+    callback("Upload started...");
+  };
 
-    request.upload.onprogress = function(event) {
-        callback(Math.round(event.loaded / event.total * 100));
-    };
+  request.upload.onprogress = function(event) {
+    callback(Math.round((event.loaded / event.total) * 100));
+  };
 
-    request.upload.onload = function() {
-        callback('progress-about-to-end');
-    };
+  request.upload.onload = function() {
+    callback("progress-about-to-end");
+  };
 
-    request.upload.onload = function() {
-        callback('progress-ended');
-    };
+  request.upload.onload = function() {
+    callback("progress-ended");
+  };
 
-    request.upload.onerror = function(error) {
-        callback('Failed to upload to server');
-    };
+  request.upload.onerror = function(error) {
+    callback("Failed to upload to server");
+  };
 
-    request.upload.onabort = function(error) {
-        callback('Upload aborted.');
-    };
+  request.upload.onabort = function(error) {
+    callback("Upload aborted.");
+  };
 
-    request.open('POST', url);
-    request.send(data);
+  request.open("POST", url);
+  request.send(data);
 }
